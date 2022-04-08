2536dcfcc46a50701af06fa8ed50be5aefd6011e
Eddy Dushime <edddush@gmail.com>
2022-04-08 12:16:02 -0400

    ada complete, testing later

--- calce.adb
+++ calce.adb
@@ -1,52 +1,133 @@
-with ada.text_io; use ada.text_io;
-with Ada.Numerics.Elementary_Functions;
-use  Ada.Numerics.Elementary_Functions;
+with Ada.Text_IO;                       use Ada.Text_IO;
+with Ada.Directories;                   use Ada.Directories;
+with Ada.Strings.Fixed;                 use Ada.Strings.Fixed;
+with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
+with Ada.Strings.Unbounded;             use Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded.Text_IO;     use Ada.Strings.Unbounded.Text_IO;
+with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
 
 procedure calce is
-    type intList is array (Positive range<>) of integer;
-    
-    procedure ecalculation(sig_digits : in integer; evalue : in out intList) is 
-        m      : integer := 4;
-        carry  : integer := 0;
-        temp   : integer := 0;
-        test_value   : float   := 0.0;
-        coeff  : intList(2..806);
+    type intList is array (Positive range <>) of Integer;
+
+    function getInput (sig_digits : out Integer) return Unbounded_String is
+        exist     : Boolean;
+        overwrite : Character;
+        file      : Unbounded_String;
+
     begin
-        test_value := (float(sig_digits + 1)) * 2.30258509;
-        
-        while float(m) * (log(float(m)) - 1.0) + 0.5 * log(6.2831852 * float(m)) < test_value loop
-            m := m + 1;
-        end loop;
+        Put_Line ("Please enter the number of significant digits: ");
+        Get (sig_digits);
+        Skip_Line;
 
-        for i in 2..m loop
-            coeff(i) := 1;
-        end loop;
+        Put_Line ("Please enter the file name for the ouput: ");
+        Get_Line (file);
+        exist := Exists (To_String (file));
 
-        for i in 1..sig_digits loop
-            if i = 1 then
-                evalue(i) := 2;
-            else
-                evalue(i) := 0;
-            end if; 
-        end loop;
+        if exist then
+            Put_Line
+               ("The file already exists, would you like to overwrite it?(Y/N)");
+            Get (overwrite);
+            Skip_Line;
+        else
+            return file;
+        end if;
 
-        for i in 2..sig_digits loop
-            carry := 0;
-           
-            for j in reverse 2..m loop
-                temp := coeff(j) * 10 + carry;
-                carry := temp/j;
-                coeff(j) := temp - carry * j;
+        if overwrite = 'Y' or overwrite = 'y' then
+            return file;
+        elsif overwrite = 'N' or overwrite = 'n' then
+            while exist loop
+                Put_Line ("Please provide a different fileName:");
+                Get_Line (file);
+                exist := Exists (To_String (file));
             end loop;
-    
-            evalue(i) := carry;
-            put(integer'image(evalue(i)));
+        end if;
+
+        return file;
+    end getInput;
+
+    procedure getestLen
+       (sig_digits : in Integer; estLen : out Integer; test : out Float)
+    is
+    begin
+        estLen := 4;
+        test   := (Float (sig_digits + 1)) * 2.302_585_09;
+
+        while Float (estLen) * (Log (Float (estLen)) - 1.0) +
+           0.5 * Log (6.283_185_2 * Float (estLen)) <
+           test
+        loop
+            estLen := estLen + 1;
         end loop;
-    end ecalculation;
-   
-    num_digits : integer;
-    evalue : intList(1..2001);
+
+    end getestLen;
+
+    function ecalcul
+       (sig_digits : in Integer; estLen : in Integer) return intList
+    is
+        carry  : Integer := 0;
+        temp   : Integer := 0;
+        evalue : intList (1 .. sig_digits);
+        coeff  : intList (2 .. estLen + 1);
+    begin
+        for i in 2 .. estLen loop
+            coeff (i) := 1;
+        end loop;
+
+        for i in 1 .. sig_digits loop
+            if i = 1 then
+                evalue (i) := 2;
+            else
+                evalue (i) := 0;
+            end if;
+        end loop;
+
+        for i in 2 .. sig_digits loop
+            carry := 0;
+
+            for j in reverse 2 .. estLen loop
+                temp      := coeff (j) * 10 + carry;
+                carry     := temp / j;
+                coeff (j) := temp - carry * j;
+            end loop;
+
+            evalue (i) := carry;
+        end loop;
+
+        return evalue;
+    end ecalcul;
+
+    --procedure to write the resulting e value to an ASCII file specified by the user
+    procedure keepe
+       (evalue : in intList; filename : Unbounded_String; size : Integer)
+    is
+        outputFp : File_Type;
+    begin
+
+        Create (outputFp, Out_File, To_String (filename));
+        Set_Output (outputFp);
+
+        for i in 1 .. size loop
+            Put (trim(Integer'Image (evalue (i)), ada.strings.both));
+            if i = 1 then
+                put(".");
+            end if;
+        end loop;
+
+        Set_Output (Standard_Output);
+
+        --close file!--
+        if Is_Open (outputFp) then
+            Close (outputFp);
+        end if;
+    end keepe;
+
+    ---start of the main wrapper program---
+    num_digits : Integer;
+    estLen     : Integer := 4;
+    test       : Float   := 0.0;
+    filename   : Unbounded_String;
 begin
-    num_digits := 801;
-    ecalculation(num_digits, evalue);
+    filename := getInput (num_digits);
+    getestLen (num_digits, estLen, test);
+    keepe (ecalcul (num_digits, estLen), filename, num_digits);
 end calce;
--- calce.f95
+++ calce.f95
@@ -1,23 +1,19 @@
 program calce    
     implicit none 
     
-    integer :: num_digits = 801
+    integer :: num_digits = 5
     integer, dimension(2001) :: evalue
-
-    call ecalculation(num_digits, evalue)
+    real    :: test_value
+    integer :: m
+    call get(num_digits, test_value, m)
+    call ecalculation(num_digits, evalue, m)
     
     contains
+    subroutine getm(sig_digits, test_value, m)
+        integer, intent(in)  :: sig_digits
+        real, intent(out)    :: test_value
+        integer, intent(out) :: m
 
-    subroutine ecalculation(sig_digits, evalue)
-        implicit none
-
-        real    :: test_value
-        integer :: coeff(2 : 806) = 1
-        integer :: temp, carry, m, i, j = 0
-        integer, intent(in) :: sig_digits   
-        integer, intent(out), dimension(sig_digits) :: evalue
-        
-        
         m = 4
         test_value = (sig_digits + 1) * 2.30258509
         
@@ -25,6 +21,27 @@ program calce
             m = m + 1
         end do
 
+    end subroutine getm
+
+    subroutine ecalculation(sig_digits, evalue, m)
+        implicit none
+        integer, intent(in) :: m
+        integer :: coeff(2 : m)
+        integer :: temp, carry, i, j, k = 0
+        integer, intent(in) :: sig_digits   
+        integer, intent(out), dimension(sig_digits) :: evalue
+        
+        
+        ! m = 4
+        ! test_value = (sig_digits + 1) * 2.30258509
+        
+        ! do while((m * (log(real(m)) - 1.0) + 0.5 * log(6.2831852 * m)) < test_value)
+        !     m = m + 1
+        ! end do
+
+        do k = 2, m
+            coeff(k) = 1
+        end do
         ! initialize every element of the evalue array as 0
         evalue = 0
 
